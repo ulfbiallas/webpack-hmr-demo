82b3572c355876b4eb1f2f24eb2b90bfab157165
ulfbiallas <ulfbiallas@googlemail.com>
2017-10-03 18:49:25 +0200

    migrate tests from mocha to jest

--- src/javascript/TestComponent.test.js
+++ src/javascript/TestComponent.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {expect} from 'chai';
 import TestComponent from './TestComponent';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<TestComponent/>', () => {
+test('<TestComponent/>', () => {
   const wrapper = shallow(
     <TestComponent/>
   );
-  expect(wrapper.find('.box').text()).to.equals('Hello');
+  expect(wrapper.find('.box').text()).toBe('Hello');
 });
